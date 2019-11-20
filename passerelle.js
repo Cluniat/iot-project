d06c9986eca6e635e695e5b7d75afbaaab449808
Philippine CLUNIAT <philippine.cluniat@gmail.com>
2019-11-20 16:01:58 +0100

    send to mosquitto ok

--- passerelle.js
+++ passerelle.js
@@ -1,38 +1,37 @@
 //N'oublie de npm install ;)
 //pour run le projet : 'node passerelle.js' dans un cmd
+
 const fetch = require('node-fetch')
 const mqtt = require('mqtt')
 
+let client = mqtt.connect({host: 'localhost', port: 1883})
 
-//Questionne le compteur
+client.on('connect', () => {
+    console.log("____Connected to mosquitto ____")
+    setInterval(publishLoop, 5000)
+})
 
-const callCounter = () => {
+const publishLoop = () => {
+    console.log("publishing to mosquitto")
     fetch('http://192.168.20.104/api/xdevices.json')
         .then(res => res.json())
         .then(json => {
-            var message = json
             console.log(json)
+            // uncomment to test mosquitto reception
+            // client.subscribe('linky')
+            client.publish('linky', JSON.stringify(json))
         })
 }
 
-callCounter()
-setTimeout(callCounter, 10000) // pour l'instant ça fait l'appel toutes les 10 secondes pour le test mais faudra mettre un truc genre toutes les 10min
+// uncomment to test mosquitto reception
 
-// Envoie la réponse au mosquitto
-//J'ai utiliser cette librairie https://github.com/mqttjs/MQTT.js
-// Mais je l'ai pas bien implémenté donc bon courage !
-host = 'localhost'
-port = 1883
-topic = 'report'
-let client = mqtt.connect(host, port)
-client.on('message', (topic, message) => {
-    client.subscribe(topic,
-        (err) => {
-            if (!err) {
-                client.publish(topic, 'coucou')
-            }
-        })
-})
+// client.on('message', (topic, message) => {
+//     console.log(message.toString())
+// })
+
+
+
+
 
 
 
